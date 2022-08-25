d59e6adc655a4d18c1739aaa40a330adef226827
thanhtu612 <dipthanht2@gmail.com>
2022-08-25 20:32:43 +0700

    init flutter_socketio

--- nodejs_sokettIO_flutter/server/index.js
+++ nodejs_sokettIO_flutter/server/index.js
@@ -1,19 +1,3 @@
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-
-io.on("connection", (socket) => {
-  // ...
-});
-const port = 3484
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
-
 // const express = require("express");
 // const { createServer } = require("http");
 // const { Server } = require("socket.io");
@@ -21,30 +5,47 @@ server.listen(port, () => {
 // const app = express();
 // const httpServer = createServer(app);
 // const io = new Server(httpServer, { /* options */ });
-
 // const port = 3484
 
-// io.on("connection", (socket) => {
-//   // ...
+// io.on("connection", (client) => {
+//   console.log("New client connected")
 // });
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://localhost:${port}/`);
+// httpServer.listen(port, () => {
+//   console.log('Example app listening at http://localhost:' + port)
 // });
 
-/*
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')();
+const server = require('http').createServer(app)
+const options = { /* ... */ }
+const io = require('socket.io')(server, options)
+const port = 3484
 
-app.get('/', (req, res) => {
-  res.send("Demo Express - NodeJS")
+io.on("connection", client => {
+  console.log("New client connected")
+  client.on('fromClient', data => {
+    console.log(data)
+    client.emit('fromServer', `${Number(data) + 1}`)
+  })
+  client.on('disconnect', () => console.log('Client disconnected'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(port, () => {
+  console.log('Example app listening at http://localhost:' + port)
 })
-*/
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send("Demo Express - NodeJS")
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 /*
 const http = require('http');
