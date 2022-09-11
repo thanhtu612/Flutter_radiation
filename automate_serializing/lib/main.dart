73739b5cc3018b722ddf3ecc8cf75e2f53055e1f
thanhtu612 <dipthanht2@gmail.com>
2022-09-11 17:28:00 +0700

    init

--- automate_serializing/lib/main.dart
+++ automate_serializing/lib/main.dart
@@ -5,14 +5,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   // runApp(const MyApp());
-  String jsonString = '{"name":"ThanhTu","email":"dipthanht@example.com"}';
+  // String jsonString = '{"name":"ThanhTu","email":"dipthanht@example.com"}';
+  String jsonString =
+      '{"name":"ThanhTu","email":"dipthanht@example.com","address":{"street":"My st.","city":"New York"}}';
   Map<String, dynamic> userMap = jsonDecode(jsonString);
   var user = User.fromJson(userMap);
 
   print('Howdy, ${user.name}!');
+  print('Address: ${user.address.street}, ${user.address.city}');
   print('We sent the verification link to ${user.email}.');
 
   user.name = 'Thanh Thanh';
+  user.address.city = "Ho Chi Minh City";
+  user.address.street = "Nguyen Thien Thuat Street";
   String json = jsonEncode(user);
   print(json);
 }
